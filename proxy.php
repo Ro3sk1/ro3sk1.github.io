1d5af882413ee67dac315fee59b66bfe21f83056
Ro3sk1 <61096405+Ro3sk1@users.noreply.github.com>
2024-02-26 22:30:48 +0000

    Add files via upload

--- proxy.php
+++ proxy.php
@@ -1,9 +1,10 @@
 <?php
-$videoUrl = 'http://213.13.16.138/shls/LIVE%24Eleven_Sports_1_HD/index.m3u8?device=TVOS_Live&start=LIVE&end=END';
+// URL of the HTTP m3u8 livestream
+$m3u8Url = 'http://213.13.16.138/shls/LIVE%24Eleven_Sports_1_HD/index.m3u8?device=TVOS_Live&start=LIVE&end=END';
 
-// Set the appropriate Content-Type header for video
-header('Content-Type: video/mp4');
+// Set the appropriate Content-Type header for m3u8
+header('Content-Type: application/vnd.apple.mpegurl');
 
-// Fetch and output the video content
-echo file_get_contents($videoUrl);
+// Fetch and output the m3u8 playlist content
+echo file_get_contents($m3u8Url);
 ?>
